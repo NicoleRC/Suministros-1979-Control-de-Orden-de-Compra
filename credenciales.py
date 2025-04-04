0568f29db80e72e18f8cb2ed853ef3c4ab33e3ce
NicoleRC <nnrc.ucv@gmail.com>
2025-04-04 16:50:37 -0400

    Actualizacion librerias puerto

--- credenciales.py
+++ credenciales.py
@@ -4,8 +4,14 @@ import pandas as pd
 
 def conexion():
     try:
+        server = st.secrets['sql_server']['server']
+        port = st.secrets['sql_server'].get('port', 1433) # Obtener el puerto, o usar 1433 por defecto
+        database = st.secrets['sql_server']['database']
+        uid = st.secrets['sql_server']['uid']
+        pwd = st.secrets['sql_server']['pwd']
+
         engine = create_engine(
-            f"mssql+pymssql://{st.secrets['sql_server']['uid']}:{st.secrets['sql_server']['pwd']}@{st.secrets['sql_server']['server']}/{st.secrets['sql_server']['database']}"
+            f"mssql+pymssql://{uid}:{pwd}@{server}:{port}/{database}"
         )
         return engine
     except Exception as e:

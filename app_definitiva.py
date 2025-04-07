2f060ea8d08e08734ebd98ac00360d2a7e463d10
NicoleRC <nnrc.ucv@gmail.com>
2025-04-07 16:55:15 -0400

    Nueva ip_publica prueba 6 (comentar formato numerico de Cant ODC)

--- app_definitiva.py
+++ app_definitiva.py
@@ -185,8 +185,8 @@ with tab1:
                 df_detalle = detalle(st.session_state["documento_seleccionado"])
 
                 # Convertir a números
-                df_detalle['Cant ODC'] = pd.to_numeric(df_detalle['Cant ODC'], errors='coerce')
-                df_detalle['Cant ODC'] = df_detalle['Cant ODC'].fillna(0)
+                #df_detalle['Cant ODC'] = pd.to_numeric(df_detalle['Cant ODC'], errors='coerce')
+                #df_detalle['Cant ODC'] = df_detalle['Cant ODC'].fillna(0)
                 
                 df_detalle['Cant REC'] = pd.to_numeric(df_detalle['Cant REC'], errors='coerce')
                 df_detalle['Cant REC'] = df_detalle['Cant REC'].fillna(0)
